0abeae730f34aa73a280be4b37224786f8e05a5e
Shamasis Bhattacharya <mail@shamasis.net>
2016-12-16 15:06:55 +0530

    Added concept of a _locals state variables to hold on to user generated variables and ignore them in subsequent runs

---
+++ test/unit/scope-locals.test.js
@@ -0,0 +1,25 @@
+describe('scope module locals', function () {
+    var Scope = require('../../'),
+        scope;
+
+    beforeEach(function () {
+        scope = Scope.create();
+        scope.set('expect', expect);
+    });
+    afterEach(function () {
+        scope = null;
+    });
+
+    it('must retain locally set globals in subsequent calls', function (done) {
+        scope.exec(`
+            expect(typeof userSetGlobal).be('undefined');
+            userSetGlobal = true;
+        `, function (err) {
+            if (err) { return done(err); }
+
+            scope.exec(`
+                expect(userSetGlobal).be(true);
+            `, done);
+        });
+    });
+});
