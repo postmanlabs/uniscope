5f039092b085e35c1ecf509ad2cbc6166a91bb1f
Postman <postman@Postmans-MacBook-Pro-2.local>
2018-10-09 12:07:09 +0530

    Updated scope options tests to chai

--- test/unit/scope-options.test.js
+++ test/unit/scope-options.test.js
@@ -1,31 +1,32 @@
-describe('scope module options', function () {
-    var Scope = require('../../');
+var Scope = require('../../');
 
-    it('must allow script execution in strict mode', function (done) {
+describe('scope module options', function () {
+
+    it('should allow script execution in strict mode', function (done) {
         var scope = Scope.create({ strict: true });
 
         scope.exec(`a = 1 + 2;`, function (err) { // eslint-disable-line quotes
-            expect(err).be.ok();
+            expect(err).to.be.an('error');
             done();
         });
     });
 
-    it('must not forward console if the option is not set', function (done) {
+    it('should not forward console if the option is not set', function (done) {
         var scope = Scope.create({}, { expect: expect }); // no options specified
-        scope.exec(`expect(typeof console).be('undefined');`, done); // eslint-disable-line quotes
+        scope.exec(`expect(typeof console).to.equal('undefined');`, done); // eslint-disable-line quotes
     });
 
-    it('must not forward console if the option is set to false', function (done) {
+    it('should not forward console if the option is set to false', function (done) {
         var scope = Scope.create({ console: false }, { expect: expect }); // specified as false
-        scope.exec(`expect(typeof console).be('undefined');`, done); // eslint-disable-line quotes
+        scope.exec(`expect(typeof console).to.equal('undefined');`, done); // eslint-disable-line quotes
     });
 
-    it('must forward native console when configured', function (done) {
+    it('should forward native console when configured', function (done) {
         var scope = Scope.create({ console: true }, { expect: expect });
-        scope.exec(`expect(console).be.an('object');`, done); // eslint-disable-line quotes
+        scope.exec(`expect(console).to.be.an('object');`, done); // eslint-disable-line quotes
     });
 
-    it('must ensure that the console forwarded is native one', function (done) {
+    it('should ensure that the console forwarded is native one', function (done) {
         var scope = Scope.create({
             console: true
         }, {
@@ -34,12 +35,12 @@ describe('scope module options', function () {
         });
 
         scope.exec(`
-            expect(console).be.an('object');
-            expect(console === refConsole).be.ok();
+            expect(console).to.be.an('object');
+            expect(console === refConsole).to.be.ok;
         `, done);
     });
 
-    it('must ensure that blocked variables are not allowed', function (done) {
+    it('should ensure that blocked variables are not allowed', function (done) {
         var scope = Scope.create({
             console: true,
             block: ['Buffer', 'myImportedGlobal']
@@ -49,8 +50,8 @@ describe('scope module options', function () {
         });
 
         scope.exec(`
-            expect(Buffer).not.be.ok();
-            expect(myImportedGlobal).not.be.ok();
+            expect(Buffer).to.be.undefined;
+            expect(myImportedGlobal).to.be.undefined;
         `, done);
     });
 });
